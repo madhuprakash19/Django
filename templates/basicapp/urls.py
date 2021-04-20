8b777d88e8a4613565ccb6904aaf653916e78382
madhuprakash19 <madhutp11@gmail.com>
2021-04-20 22:08:13 +0530

    New Template features

---
+++ templates/basicapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from basicapp import views
+
+#template_tagging
+app_name = 'basicapp' 
+
+urlpatterns =[
+    path('other',views.other,name = 'other'),
+    path('relative',views.relative,name = 'relative'),
+]
